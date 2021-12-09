536fdde3e9b14b193314a239953d48f606efe4ad
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-12-09 19:35:10 +0100

    adjusted paths

--- main.py
+++ main.py
@@ -96,12 +96,13 @@ os.makedirs('forecasts', exist_ok=True)
 # Download all forecasts changed in the PR into the forecasts folder that have not been deleted
 for f in forecasts:
     if f.status != "removed":
-        urllib.request.urlretrieve(f.raw_url, f"forecasts/{f.filename.split('/')[-1]}")
+        urllib.request.urlretrieve(f.raw_url, f"forecasts/{f.filename}")
     
 # Run validations on each file that matches the naming convention
 all_errors = {}
 
-for file in glob.glob("forecasts/*.csv"):
+for file in glob.glob("forecasts/**/**/*.csv"):
+    print('file: ', file)
     errors = check_forecast(file)
     if len(errors) > 0:
         all_errors[os.path.basename(file)] = errors
